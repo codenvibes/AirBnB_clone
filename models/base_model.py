22408ff8ae7234518908930e03e1150767bf81d8
amoskarugo <karugoamos@gmail.com>
2023-10-12 13:27:12 +0000

    Airbnb

---
+++ models/base_model.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+"""Base model module that defines a class
+    that serves as base of other classes
+"""
+import uuid
+from datetime import time, datetime
+import models
+
+class BaseModel:
+    """Base model class that defines all common attributes/methods
+        for other classes.
+    """
+
+    def __init__(self, *args, **kwargs):
+
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key in ["created_at", "updated_at"]:
+                    setattr(self, key, value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+
+    def __str__(self):
+        return "[{}] ({}) <{}>".format(self.__class__.__name__, self.id, self.__dict__)
+
+
+    def save(self):
+        """ updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.created_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
+
+all_objs = models.storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""FileStorage module for serialization and deserialization"""
+
+import json
+
+
+class FileStorage:
+    """class FileStorage that serializes instances to a
+     JSON file and deserializes JSON file to instances:
+     """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """appends new object to the dictionary __objects
+
+        Args:
+            obj (object): object to be added
+
+        """
+
+        class_name = obj.__class__.__name__
+        self.__objects[f"{class_name}.{obj.id}"] = obj
+
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path)"""
+
+        objects_dict = {}
+
+        for key, obj in self.__objects.items():
+            objects_dict[key] = obj.to_dict()
+
+        with open(self.__file_path, "w") as file_storage:
+            json.dump(objects_dict, file_storage)
+
+    def reload(self):
+        """deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ;
+        otherwise, do nothing. If the file doesn’t exist,
+         no exception should be raised)"""
+
+        try:
+            with open(self.__file_path, "r") as file_storage:
+                objects_dict = json.load(file_storage)
+                for key, value in objects_dict.items():
+                    class_name, id = key.split(".")
+                    self.__objects[f"{key}"] = globals()[class_name](**value)
+
+        except FileNotFoundError:
+            pass
+
+
